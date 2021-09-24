1df4d0e698575d0186d2cf333de399e5fd4f9f13
martinfrola <martinfrola@gmail.com>
2021-09-24 17:09:54 -0300

    Se agregó google analytics

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./sass/app.scss";
-import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,7 +12,7 @@ ReactDOM.render(
   document.getElementById("root")
 );
 //Inicación de firebase
-const firebaseApp = initializeApp({
+var firebaseConfig = {
   apiKey: "AIzaSyBvM5i09B_UQoHqE2fcUNVn9t9htisphr4",
   authDomain: "tench-arg.firebaseapp.com",
   projectId: "tench-arg",
@@ -20,7 +20,8 @@ const firebaseApp = initializeApp({
   messagingSenderId: "388159337238",
   appId: "1:388159337238:web:2d9431aa61d31ff44dd637",
   measurementId: "G-TME7LQNXMX",
-});
+};
 
-const app = initializeApp(firebaseApp);
-const analytics = getAnalytics();
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export default analytics;
